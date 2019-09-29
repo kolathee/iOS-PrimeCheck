09a0bf34d9ee4f0a7585fa1450f7c4085d429851
Kolathee Payuhawatthana <kolathee@icloud.com>
2019-09-29 23:59:03 +0700

    Fix bug (check prime function)

--- YelloTalkPrimeChecker/Extension/+ Int.swift
+++ YelloTalkPrimeChecker/Extension/+ Int.swift
@@ -11,11 +11,14 @@ import Foundation
 extension Int {
 
     func isPrime() -> Bool {
-        
         /// Basic Check
         if self <= 1 { return false }
         if self == 2 { return true }
         if self == 3 { return true }
+        if self == 5 { return true }
+        if self == 7 { return true }
+        if self == 11 { return true }
+        if self == 13 { return true }
         
         /// and then check 3 to √n
         let endNumber = Int(Double(self).squareRoot())
