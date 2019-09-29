7f9d3c1100e588f78cdde5de131c8efa302c7e6c
Kolathee Payuhawatthana <kolathee@icloud.com>
2019-09-29 15:04:32 +0700

    Create HomeViewController, HomeViewModel

---
+++ YelloTalkPrimeChecker/Controller/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  YelloTalkPrimeChecker
+//
+//  Created by Kolathee Payuhawatthana on 29/9/19.
+//  Copyright © 2019 Kolathee Payuhawatthana. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ YelloTalkPrimeChecker/Controller/Home/HomeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewModel.swift
+//  YelloTalkPrimeChecker
+//
+//  Created by Kolathee Payuhawatthana on 29/9/19.
+//  Copyright © 2019 Kolathee Payuhawatthana. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewModel {
+    
+}
