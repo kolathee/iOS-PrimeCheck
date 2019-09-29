ee4b1073e99af4a97c8935c7155f51f8896f76ef
Kolathee Payuhawatthana <kolathee@icloud.com>
2019-09-29 22:31:27 +0700

    Add check prime function

---
+++ YelloTalkPrimeChecker/Extension/+ Int.swift
@@ -0,0 +1,28 @@
+//
+//  + Int.swift
+//  YelloTalkPrimeChecker
+//
+//  Created by Kolathee Payuhawatthana on 29/9/19.
+//  Copyright © 2019 Kolathee Payuhawatthana. All rights reserved.
+//
+
+import Foundation
+
+extension Int {
+
+    func isPrime() -> Bool {
+        
+        /// Basic Check
+        if self <= 1 { return false }
+        if self == 2 { return true }
+        if self == 3 { return true }
+        
+        /// and then check 3 to √n
+        let endNumber = Int(Double(self).squareRoot())
+        for checker in 4...endNumber {
+            if self % checker == 0 { return false }
+        }
+        return true
+    }
+    
+}
