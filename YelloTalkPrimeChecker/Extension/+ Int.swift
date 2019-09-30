5921c164b08bf129bea3015b81f0510a63d9dc65
Kolathee Payuhawatthana <kolathee@icloud.com>
2019-09-30 13:04:21 +0700

    Improve check prime algorithm efficiency & Fix some bugs

--- YelloTalkPrimeChecker/Extension/+ Int.swift
+++ YelloTalkPrimeChecker/Extension/+ Int.swift
@@ -15,14 +15,14 @@ extension Int {
         if self <= 1 { return false }
         if self == 2 { return true }
         if self == 3 { return true }
-        if self == 5 { return true }
-        if self == 7 { return true }
-        if self == 11 { return true }
-        if self == 13 { return true }
+        if self % 2 == 0 { return false }
+        if self % 3 == 0 { return false }
+        if (self % 5 == 0) && (self != 5) { return false }
         
-        /// and then check 3 to √n
-        let endNumber = Int(Double(self).squareRoot())
+        /// and then check 4 to √n
+        let endNumber = (self < 16) ? (self - 1) : Int(Double(self).squareRoot())
         for checker in 4...endNumber {
+            print(checker)
             if self % checker == 0 { return false }
         }
         return true
